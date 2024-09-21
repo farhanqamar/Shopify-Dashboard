b162b276c0f61d6f19ee9f4966f502c13960733d
Jaisha <jaisha@northrays.com>
2024-09-21 12:51:36 +0500

    updated

--- src/Components/TrackQuantity.tsx
+++ src/Components/TrackQuantity.tsx
@@ -1,15 +1,15 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const TrackQuantity = () => {
-    const [isChecked, setIsChecked] = useState(true); 
+  const [isChecked, setIsChecked] = useState(true);
 
-    const handleCheckboxChange = () => {
-      setIsChecked(!isChecked);
-    };
-    return (
-      <section>
-          <div className='  bg-white rounded-xl p-4 space-y-4' >
-      <h1>Inventory</h1>
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+  return (
+    <section>
+      <div className='  bg-white rounded-xl p-4 space-y-4' >
+        <h1 className='font-bold text-base'>Inventory</h1>
         <div className='border-b space-y-2'>
           <input
             type="checkbox"
@@ -22,23 +22,23 @@ const TrackQuantity = () => {
           </label>
           <h1 className='font-bold'>Quality</h1>
         </div>
-  
+
         {isChecked && (
-         <div className='flex space-x-2'>
-           <input
-            type="checkbox"
-           
+          <div className='flex space-x-2'>
+            <input
+              type="checkbox"
+
             // className=' border border-black outline-none w-full rounded-lg'
             />
             <label htmlFor="">Continue selling when out of stock</label>
             {/* <label htmlFor="">This won't affect Shopify POS. Staff will see a warning, but can complete sales when available inventory reaches zero and below.</label> */}
-         </div>
-          )}
+          </div>
+        )}
       </div>
-            
-          
-      </section>
-    )
+
+
+    </section>
+  )
 }
 
 export default TrackQuantity
